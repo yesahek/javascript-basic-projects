0bed98819f96cb485eb993c2c90e05591219e04a
yesahek <yesahek@gmail.com>
2022-10-30 23:57:25 +0300

    questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,21 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+//const question = document.querySelectorAll('.question');
+
+//btn.addEventListener("click", function(){
+    // if(question.classList.contains("show-text")){
+    //     question.classList.remove("show-text");
+    // }else{
+    //     question.classList.add("show-text");
+    // }
+    //question.classList.toggle("show-text");
+//});
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+const question = e.currentTarget.parentElement.parentElement;
+question.classList.toggle("show-text");
+});
+});
